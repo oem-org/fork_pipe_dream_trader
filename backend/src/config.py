71df1632cc26669a382c12d775b1ffc4d591ab87
JeppeOEM <jappa669@gmail.com>
2024-11-12 18:40:20 +0100

    fixed folder structure

--- backend/src/config.py
+++ backend/src/config.py
@@ -4,9 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
-    CELERY_BROKER_URL: str
-    CELERY_RESULT_BACKEND: str
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    model_config = SettingsConfigDict(env_file="testenv", extra="ignore")
 
 
 Config = Settings()
