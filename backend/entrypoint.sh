152f7d4261a78e45b4803af490b62ca5fbeee904
JeppeOEM <jappa669@gmail.com>
2024-11-13 22:47:41 +0100

    testing

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,26 +6,8 @@ case "$RTE" in
         alembic upgrade head && uvicorn src.main:app --host 0.0.0.0 --port 8000 --reload
         ;;
     test )
-        echo "** Testing locally mode."
-        echo "** Testing locally mode."
-        echo "** Testing locally mode."
-        echo "** Testing locally mode."
-        echo "** Testing locally mode."
-        echo "** Testing locally mode."
-        echo "** Testing locally mode."
-        echo "** Testing locally mode."
-        echo "** Testing locally mode."
-        echo "** Testing locally mode."
-        echo "** Testing locally mode."
-        echo "** Testing locally mode."
-        echo "** Testing locally mode."
-        echo "** Testing locally mode."
-        echo "** Testing locally mode."
-        echo "** Testing locally mode."
-        echo "** Testing locally mode."
-        echo "** Testing locally mode."
         pytest
-        pip-audit || exit 1
+        # pip-audit || exit 1
         ;;
 
     prod )
