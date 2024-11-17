bf7a272760e6a383473567bd01352a6524782b0d
JeppeOEM <jappa669@gmail.com>
2024-11-17 20:42:38 +0100

    updated folder structure

---
+++ frontend/app/components/shared/icons/IconPropsInterface.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export interface IconPropsInterface extends React.SVGProps<SVGSVGElement> {
+  // Optional className property for styling the icon
+  className?: string;
+}
---
+++ frontend/app/components/shared/navigation/top-nav.tsx
@@ -0,0 +1,9 @@
+import Logo from "@/app/components/shared/logo";
+
+export default function TopNav() {
+  return (
+    <nav>
+      <Logo />
+    </nav>
+  );
+}
