4a533bb2d7400e1ddf2b113ccad723e6852c9035
JeppeOEM <jappa669@gmail.com>
2024-11-21 00:03:01 +0100

    add chart

--- frontend/app/strategy/chart/page.tsx
+++ frontend/app/strategy/chart/page.tsx
@@ -1,3 +1,27 @@
+import { PriceChart } from "@/app/components/strategy/price-chart";
+
+const { data: dataPrices, error: errorPrices, isLoading: isLoadingPrices } = usePriceQuery(selectedCoinId)
+
 export default function Page() {
-	return <p>chart</p>;
+
+	const initialData = [
+		{ time: '2018-12-22', value: 32.51 },
+		{ time: '2018-12-23', value: 31.11 },
+		{ time: '2018-12-24', value: 27.02 },
+		{ time: '2018-12-25', value: 27.32 },
+		{ time: '2018-12-26', value: 25.17 },
+		{ time: '2018-12-27', value: 28.89 },
+		{ time: '2018-12-28', value: 25.46 },
+		{ time: '2018-12-29', value: 23.92 },
+		{ time: '2018-12-30', value: 22.68 },
+		{ time: '2018-12-31', value: 22.67 },
+	]
+
+
+	return (
+		<div>
+			<PriceChart />
+		</div>
+
+	)
 }
