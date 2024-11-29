b28c27d3738c0d37359a3c5bb30fa3b96e5c7b12
JeppeOEM <jappa669@gmail.com>
2024-11-29 02:36:50 +0100

    clean up

--- backend/src/dependencies.py
+++ backend/src/dependencies.py
@@ -1,25 +1,13 @@
 from typing import Annotated
 
 from fastapi import Depends
-# from fastapi.security import OAuth2PasswordBearer
-# from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 
 from src.services.TimescaleService import TimescaleService, get_timescale_service
 from .orm_connection import get_db
 from .services.auth.auth_services import get_current_user
-# bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-# tokenUrl is relative to the url of the api, and makes it visible in OpenAPI documentation
-# will look for Authorization header, check if the value is Bearer plus has token.
-# oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
-# SQLAlchemy uses "Connection Pooling"
-# With using yield a single session is created per request
-# If yield was ommited it woul
-#https://fastapi.tiangolo.com/tutorial/dependencies/dependencies-with-yield/
-
- 
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
