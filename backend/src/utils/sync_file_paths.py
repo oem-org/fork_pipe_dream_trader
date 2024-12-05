10d3df6716ab7cbb1bb2129cf169c2cd19b42bac
JeppeOEM <jappa669@gmail.com>
2024-12-05 18:24:16 +0100

    add or delete to db

--- backend/src/utils/sync_file_paths.py
+++ backend/src/utils/sync_file_paths.py
@@ -4,28 +4,53 @@ from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
 from ..models import Files
+from ..schemas import FileTypeEnum
+
+
+def delete_missing_files(db: Session, missing_files: dict):
+    files_to_delete = []
+    for filename in missing_files.keys():
+        db_file_to_delete = db.query(Files).filter(Files.filename == filename).first()
+        if db_file_to_delete:
+            files_to_delete.append(db_file_to_delete)
+
+    for file_to_delete in files_to_delete:
+        db.delete(file_to_delete)
+
+    db.commit()
+
+
+def add_non_matching_files(db: Session, non_matching_files: dict):
+    files_to_add = []
+    for filename, file_info in non_matching_files.items():
+        file_to_add = Files(
+            filename=filename,
+            path=file_info["folder_path"],
+            file_type=FileTypeEnum.UNKNOWN,
+        )
+        files_to_add.append(file_to_add)
+
+    for file_to_add in files_to_add:
+        db.add(file_to_add)
+
+    db.commit()
 
 
 def sync_file_paths(db: Session):
-    """
-    Checks if the files in the specified folder match the files stored in the database.
-    """
     print("syncing")
     current_directory = Path(__file__).parent
     parent_parent_folder = current_directory.parent.parent
     folder_path = parent_parent_folder / "uploaded_files"
 
-    # List all files in the folder
     folder_files = {file.name: file for file in folder_path.iterdir() if file.is_file()}
-
     print(folder_files)
 
     db_files = db.query(Files).all()
-    # Convert string to a pathlib path
     db_file_map = {db_file.filename: Path(db_file.path) for db_file in db_files}
 
     matching_files = {}
     non_matching_files = {}
+    missing_in_folder = {}
 
     for filename, file_path in folder_files.items():
         if filename in db_file_map:
@@ -47,6 +72,9 @@ def sync_file_paths(db: Session):
         for db_file in db_files
         if db_file.filename not in folder_files
     }
+
+    delete_missing_files(db, missing_in_folder)
+    add_non_matching_files(db, non_matching_files)
 
     return {
         "matching_files": matching_files,
