b025b94f684d10f2f3bb0a530e89335682f7b82b
JeppeOEM <jappa669@gmail.com>
2024-12-25 16:40:19 +0100

    codition types

--- frontend/src/interfaces/Condition.ts
+++ frontend/src/interfaces/Condition.ts
@@ -1,5 +1,16 @@
+
 export interface Condition {
-    indicator?: string | null;
-    operator?: string | null;
-    value?: number | null;
+    indicator: string | null;
+    operator: string;
+    value: number | string | { indicator: string };
 }
+
+
+export type ConditionGroup = Condition[];
+
+
+export type LogicalOperator = "&" | "|" | "~";
+
+
+export type ConditionsArray = Array<ConditionGroup | LogicalOperator>;
+
