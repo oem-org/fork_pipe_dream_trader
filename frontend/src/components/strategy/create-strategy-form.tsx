7eefec8f078912df9cbd967c711807c6241ed798
JeppeOEM <jappa669@gmail.com>
2025-01-04 01:01:48 +0100

    clean up

--- frontend/src/components/strategy/create-strategy-form.tsx
+++ frontend/src/components/strategy/create-strategy-form.tsx
@@ -4,75 +4,59 @@ import getFilesQuery from "@/lib/hooks/react-query/getFilesQuery";
 import { File } from "@/interfaces/File";
 import { postStrategyApi } from "@/lib/apiClientInstances";
 import { useNavigate } from 'react-router-dom';
-import { DataSourceEnum } from "@/interfaces/enums/DataSourceEnum";
-import { DatabaseSource, FileSource } from "@/interfaces/Strategy";
 import useStrategyStore from "@/lib/hooks/stores/useStrategyStore";
-import { useQueryClient } from "@tanstack/react-query";
-
 
 export default function CreateStrategyForm() {
 	const [name, setName] = useState("");
 	const [description, setDescription] = useState("");
 	const [fileId, setFileId] = useState<number>(0);
-	const [dataSourceType, setDataSourceType] = useState<DataSourceEnum>(DataSourceEnum.FILE);
 	const { data: dataFiles } = getFilesQuery();
 	const { setStrategyId } = useStrategyStore()
-	const queryClient = useQueryClient();
-	const [databaseOption, setDatabaseOption] = useState("");
 	const [errors, setErrors] = useState({
 		name: "",
 		description: "",
 		fileId: "",
-		databaseOption: "",
-
 	});
 
 	const [touched, setTouched] = useState({
 		name: false,
 		description: false,
 		fileId: false,
-		databaseOption: false,
 	});
 
 	const navigate = useNavigate();
-
 
 	const validateForm = () => {
 		const newErrors = {
 			name: touched.name && !name ? "Strategy name is required" : "",
 			description: touched.description && !description ? "Strategy description is required" : "",
-			fileId: touched.fileId && dataSourceType === DataSourceEnum.FILE && !fileId ? "Select a file" : "",
-			databaseOption: touched.databaseOption && dataSourceType === DataSourceEnum.DATABASE && !databaseOption
-				? "Please enter a database option" : "",
+			fileId: touched.fileId && !fileId ? "Select a file" : "",
 		};
 		setErrors(newErrors);
 		return Object.values(newErrors).every((error) => error === "");
 	};
-	// TODO: clean source type
+
 	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		setTouched({
 			name: true,
 			description: true,
 			fileId: true,
-			databaseOption: true,
 		});
 		if (!validateForm()) return;
 
-		// Timescale Database implemention not finished
-
 		try {
-				const strategy = await postStrategyApi.post({
-					name,
-					fk_file_id: fileId,
-					description,
-				});
-				setStrategyId(strategy.id)
+			const strategy = await postStrategyApi.post({
+				name,
+				fk_file_id: fileId,
+				description,
+			});
+			setStrategyId(strategy.id)
 
-				navigate(`/strategy/${strategy.id}`);
-			}
+			navigate(`/strategy/${strategy.id}`);
+		}
 
-		 catch (error) {
+		catch (error) {
 			console.error("Error creating strategy:", error);
 		}
 	};
@@ -82,8 +66,6 @@ export default function CreateStrategyForm() {
 			Create New Strategy
 		</h2>
 		<form onSubmit={handleSubmit} className="space-y-4 ">
-
-
 			<div>
 				<GenericSelect<File>
 					data={dataFiles || []}
