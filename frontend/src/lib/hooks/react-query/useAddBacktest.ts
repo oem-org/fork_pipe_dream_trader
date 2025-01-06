5e22974cb0daa7d47ea3f169c193fcb023f80014
JeppeOEM <jappa669@gmail.com>
2025-01-06 21:52:33 +0100

    going back to old dockerfile

--- frontend/src/lib/hooks/react-query/useAddBacktest.ts
+++ frontend/src/lib/hooks/react-query/useAddBacktest.ts
@@ -1,31 +1,29 @@
 
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import Backtest from "@/interfaces/Backtest";
-import { postStrategyBacktestsApi } from "@/lib/apiClientInstances";
+// import { useMutation, useQueryClient } from "@tanstack/react-query";
+// import { CreateBacktestRequest } from "@/interfaces/Backtest";
+// import { postStrategyBacktestApi } from "@/lib/apiClientInstances";
 
-const addBacktest = async (indicator: Backtest, paramId: number): Promise<any> => {
-	console.log("adding this indicator", indicator)
-	const response = await postStrategyBacktestsApi.post(paramId, {
-		fk_indicator_id: indicator.id,
+// const addBacktest = async (indicator: CreateBacktestRequest, paramId: number): Promise<any> => {
+// 	console.log("adding this indicator", indicator)
+// 	const response = await postStrategyBacktestApi.post(paramId, {
+// 		"buy_conditions"
+// 	});
+// 	return response.data;
+// };
 
-		settings: indicator.default_settings,
-	});
-	return response.data;
-};
+// export const useAddBacktest = (paramId: number) => {
+// 	const queryClient = useQueryClient();
 
-export const useAddBacktest = (paramId: number) => {
-	const queryClient = useQueryClient();
-
-	return useMutation({
-		mutationFn: (indicator: Backtest) => addBacktest(indicator, paramId),
-		onSuccess: () => {
-			// Invalidate cache to refetch indicators
-			queryClient.invalidateQueries({ queryKey: ["strategyBacktests"] });
-			return true;
-		},
-		onError: (error: unknown) => {
-			console.error("Failed to add indicator:", error);
-			return false
-		},
-	});
-};
+// 	return useMutation({
+// 		mutationFn: (indicator: CreateBacktestRequest) => addBacktest(indicator, paramId),
+// 		onSuccess: () => {
+// 			// Invalidate cache to refetch indicators
+// 			queryClient.invalidateQueries({ queryKey: ["strategyBacktests"] });
+// 			return true;
+// 		},
+// 		onError: (error: unknown) => {
+// 			console.error("Failed to add indicator:", error);
+// 			return false
+// 		},
+// 	});
+// };
